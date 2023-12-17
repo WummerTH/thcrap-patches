c5e76444ae7da3e0a9e05ef69847a909681e685d
Wummer <wummer.dev@proton.me>
2023-12-17 16:26:21 +0100

    Add 'Legacy of Normal Kingdom' patch

---
+++ lonk/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro",
+    "nmlgc/script_latin",
+    "nmlgc/western_name_order",
+    "lang_en"
+  ],
+  "id": "lonk",
+  "title": "(TH15) Replaces occurences of difficulty names in dialogue with 'Normal'"
+}
--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
     "title": "Wummer's patch repository",
     "patches": {
         "gachi_bgm": "Replaces some of the BGM with Gachimuchi remixes",
-        "lang_en-wummer": "(TH11 ReimuA route) Alternative English language pack by Wummer"
+        "lang_en-wummer": "(TH11 ReimuA route) Alternative English language pack by Wummer",
+        "lonk": "(TH15) Replaces occurences of difficulty names in dialogue with 'Normal'"
     }
 }
